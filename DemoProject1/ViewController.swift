ea45549d3f9c6f187a7bd962d1427f486e881c16
Nick <Nicholasgilbert21@yahoo.com>
2021-02-11 08:10:15 -0500

    First Commit

--- DemoProject1/ViewController.swift
+++ DemoProject1/ViewController.swift
@@ -13,6 +13,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let datePicker : UIDatePicker = UIDatePicker()
+        
+        
+        datePicker.frame = CGRect(x:10, y:100, width: self.view.frame.width, height: 200)
+        
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.backgroundColor = UIColor.white
+
+        
+        //datePicker.addTarget(self, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+        
+        self.view.addSubview(datePicker)
+    }
+    
+    func datePickerValueChanged(_ sender: UIDatePicker){
+        let dateFormatter: DateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "MM/dd/yyyy hh:mm a"
+        
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        
+        print("Selected value \(selectedDate)")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
